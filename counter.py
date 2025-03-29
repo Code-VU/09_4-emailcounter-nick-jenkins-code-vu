eeef7091e0bb8c105726abf1caed0eac184d8d15
Nick Jenkins <Nick.Jenkins@vu.com>
2025-03-28 20:29:58 -0500

    counter.py jenkins

--- counter.py
+++ counter.py
@@ -2,7 +2,36 @@ def countEmail():
     # This first line is provided for you
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
+   
+    mbox_file = open(name)
+    fromdict = {}
+
+    for line in mbox_file: 
+        line = line.strip()
+        
+        if line.startswith("From "):
+            sender = line.split()[1]
+
+            if sender not in fromdict:
+                fromdict[sender] = 1
+            else:
+                fromdict[sender] += 1
+    dictvals = fromdict.values()
+    maxvals = max(dictvals)
+
+    print(max(fromdict, key = fromdict.get),maxvals)
+
+            
+
+
+
+
+#need to make a dictinary 
+#sort though the file
+# count what we sorted thorugh 
+# the program looks for From 
+#takes from the 2nd word (3)
+
         
 
 ## if you want to test locally run > python counter.py
